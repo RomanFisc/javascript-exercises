29c2b827822b4c7b8a49ab112d0f2a99294fe993
RomanFisc <romanfisc@gmail.com>
2023-11-15 16:20:35 -0500

    Passed 1st test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
-};
+
+const repeatString = function(str, num) {
+    let repeatedString = 'hey';
+    for (let i = 0; i < 2; i++) {
+        repeatedString += str;
+    }
+    return repeatedString;
+}
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+//npm test repeatString.spec.js
