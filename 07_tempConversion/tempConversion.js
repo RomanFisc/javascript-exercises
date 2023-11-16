f45813d8182484151e23c450017ffa20c85788c6
RomanFisc <romanfisc@gmail.com>
2023-11-16 10:39:25 -0500

    passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  let number = celsius; 
+  rounded = Math.round(number * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  let number = fahrenheit; 
+  rounded = Math.round(number * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
@@ -9,3 +17,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+// npm test tempConversion.spec.js
