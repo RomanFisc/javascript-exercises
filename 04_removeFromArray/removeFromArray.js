f04d5f3d0fd91ada30f43108278a193e52cfcdd4
RomanFisc <romanfisc@gmail.com>
2023-11-15 22:36:22 -0500

    Passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
 
+
+
+const removeFromArray = function(array, ...newArray) {
+    let a = array;
+    let b = newArray;
+    let c = a.filter(d => !b.includes(d));
+    return c;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// npm test removeFromArray.spec.js
+
+
+//myArray.splice(2,1); passed first test but not multiple
+//update: 
