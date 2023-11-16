a1b7a46a529355b5366cc9d623167f1ed20f4c5c
RomanFisc <romanfisc@gmail.com>
2023-11-15 21:41:51 -0500

    Uploaded, passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+//
+const reverseString = function(str) {
+    let splitString = str.split('');
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join('');
+    return joinArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//npm test reverseString.spec.js
