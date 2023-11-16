06d45e5e9c7f182056cb9a93b7bc4e8b4d8af7f2
RomanFisc <romanfisc@gmail.com>
2023-11-15 23:56:52 -0500

    Passes with all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    let biggerNum = Math.max(num1,num2);
+    let smallerNum = Math.min(num1,num2);
 
-};
+    if (typeof num1 === "string" || typeof num2 === "string") {
+        return "ERROR";
+    }
+    else if ((biggerNum >=0) && (smallerNum >=0)) {
+        for (let i = smallerNum; i <= biggerNum; i++) {
+        finalSum = finalSum + i;
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    console.log(finalSum);
+    return finalSum;
+
+}
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// npm test sumAll.spec.js
