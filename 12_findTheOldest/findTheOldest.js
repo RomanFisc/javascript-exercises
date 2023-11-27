849299d735573b0d7a7bd216d4b6c97821c0ebff
RomanFisc <romanfisc@gmail.com>
2023-11-27 15:10:12 -0500

    Passes all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let maxAge = 0; 
+    let oldestPerson = {};
 
+    people.forEach(person => {
+    let age;
+    if (person.yearOfDeath) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = currentYear - person.yearOfBirth;
+    }
+    if (age > maxAge) {
+        maxAge = age;
+        oldestPerson = person;
+    }
+    });
+    return oldestPerson
 };
 
+
+//npm test findTheOldest.spec.js
 // Do not edit below this line
 module.exports = findTheOldest;
+
