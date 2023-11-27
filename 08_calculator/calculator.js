fbbbddcb80c7e8fd62aa920b0cdb15e3aebc575b
RomanFisc <romanfisc@gmail.com>
2023-11-27 12:21:15 -0500

    all solutions working

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((currentValue, totalValue) => (currentValue + totalValue), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((currentValue, totalValue) => (currentValue * totalValue) );
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1; 
+  }
+  for (var i = num -1; i >= 1; i-- ) {
+    num *= i;
+  }
+  return num;
 };
+
+
+
+//npm test calculator.spec.js
 
 // Do not edit below this line
 module.exports = {
