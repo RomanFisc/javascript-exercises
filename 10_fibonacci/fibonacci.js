5184fb2ab76ae08798fae12dec99ea9d76d4d19a
RomanFisc <romanfisc@gmail.com>
2023-11-27 14:10:15 -0500

    Passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    var num1 = 0;
+    var num2 = 1;
+    var sum; 
+    var i = 0;
+    if (num >= 0) {
+        for (i = 1; i < num; i++) { 
+            sum = num1 + num2;
+            num1 = num2; 
+            num2 = sum;
+        }
+    }   
+    else { 
+        return "OOPS";
+    }
+    return num2;
 };
 
+
+// npm test fibonacci.spec.js
 // Do not edit below this line
 module.exports = fibonacci;
