64069e7e366a4eb996eb33dce073d73b15ea320c
RomanFisc <romanfisc@gmail.com>
2023-11-27 13:12:39 -0500

    Solution passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join('');
 
+    return reverseStr === lowRegStr;
 };
 
+
+
+//input desired string 
+//if the first letter === last letter, continue 
+//else if the second letter === second to last letter, continue 
+// else if the third letter === 
+// else return false
+
+//npm test palindromes.spec.js
 // Do not edit below this line
 module.exports = palindromes;
